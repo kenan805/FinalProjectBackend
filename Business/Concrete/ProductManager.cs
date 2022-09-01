b70ba8bc0dc6b1265f7cf6996e37af39834d39d7
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-09-02 00:45:10 +0400

    Generic  repository pattern and Entity Framework Core added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,5 +19,9 @@ namespace Business.Concrete
         }
 
         public List<Product> GetAll() => _productDal.GetAll();
+
+        public List<Product> GetAllByCategoryId(int id) => _productDal.GetAll(p => p.CategoryId == id);
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max) => _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,7 +33,17 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(removedProduct);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll() => _products;
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
 
         public List<Product> GetAllByCategory(int categoryId) => _products.Where(p => p.CategoryId == categoryId).ToList();
 
